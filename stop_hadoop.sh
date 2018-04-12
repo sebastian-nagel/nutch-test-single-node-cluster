3a06896c597e9843537031035b180dd0127cf4a1
Sebastian Nagel <snagel@apache.org>
2018-04-12 14:49:44 +0200

    stop Hadoop services

--- stop_hadoop.sh
+++ stop_hadoop.sh
@@ -5,6 +5,6 @@ source hadoop_install_config.sh
 set -e
 
 # launch Hadoop services
-$HADOOP_HOME/sbin/start-dfs.sh 
-$HADOOP_HOME/sbin/start-yarn.sh
+$HADOOP_HOME/sbin/stop-dfs.sh
+$HADOOP_HOME/sbin/stop-yarn.sh
 
